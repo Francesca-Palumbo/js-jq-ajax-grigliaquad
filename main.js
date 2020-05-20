aa2f85564e638f39bcddf0a4ccc0ef25693a0884
Francesca Palumbo <econofra87@gmail.com>
2020-05-20 10:37:49 +0200

    fine

--- main.js
+++ main.js
@@ -5,69 +5,51 @@
 // BONUS: generare la griglia in jQuery utilizzando handlebars
 
 $(document).ready(function() {
+
+    // generare la griglia in jQuery utilizzando handlebars
+    var html_template = $('#entry-template').html();
+    var template_function = Handlebars.compile(html_template);
+
+    // creo un ciclo per generare la griglia composta da 36 quadrati
     for (var i = 0; i < 36; i++) {
-        $('.griglia').append('<div class="quadrato"></div>')
+
+        // griglia con handlebars
+        var html_finale = template_function({});
+        $('.griglia').append(html_finale);
+
+        // griglia senza handlebars
+        // $('.griglia').append('<div class="quadrato"></div>')
     }
 
-    $('.quadrato').each(function(){
-        // faccio una chiamata ajax per generare un numero casuale => giocata pc
+    // apro la funzione con il click su quadrato
+    $('.quadrato').click(function(){
+
+        // salvo il riferimento al quadrato su cui ho cliccato
+        var that = $(this);
+
+        // faccio una chiamata ajax per generare un numero random tra 1 e 9
             $.ajax({
                 'url': 'https://flynn.boolean.careers/exercises/api/random/int',
                 'method': 'GET',
                 'success': function(data) {
-                    // recupero il numero restituito dall'api
-                    var numero_pc = data.response;
-                    console.log('Numero pc: ' + numero_pc);
 
-                    $(numero_pc).show();
+                    // recupero il numero restituito dall'api
+                    var numero_generato = data.response;
+                    console.log(numero_generato);
+                    // console.log('Numero generato: ' + numero_pc);
+
+                    // se il numero generato è <=5 , il quadrato si colorerà di giallo
+                    if (numero_generato <= 5) {
+                        $(that).addClass('giallo');
+                    // se il numero generato è >5 , il quadrato si colorerà di verde
+                    } else {
+                        $(that).addClass('verde')
+                    }
+                    that.text(numero_generato);
                 },
                 'error': function() {
                     alert('si è verificato un errore');
                 }
             });
-
-            if (numero_pc <= 5) {
-                $(numero_pc).addClass('giallo');
-            } else {
-                $(numero_pc).addClass('verde')
-            }
     });
-
-
-
 });
-
-// $(document).ready(function() {
-//
-//     // quando si carica la pagina chiedo un numero all'utente => giocata utente
-//     var numero_utente = parseInt(prompt('Inserisci un numero tra 1 e 9'));
-//     console.log('Numero utente: ' + numero_utente);
-//
-//     // faccio una chiamata ajax per generare un numero casuale => giocata pc
-//     $.ajax({
-//         'url': 'https://flynn.boolean.careers/exercises/api/random/int',
-//         'method': 'GET',
-//         'success': function(data) {
-//             // recupero il numero restituito dall'api
-//             var numero_pc = data.response;
-//             console.log('Numero pc: ' + numero_pc);
-//             // invoco la funzione per stabilire il vincitore, passando i due numeri
-//             decreta_vincitore(numero_utente, numero_pc);
-//         },
-//         'error': function() {
-//             alert('si è verificato un errore');
-//         }
-//     });
-//
-// });
-
-// funzione che riceve come parametri due numeri
-// che rappresentano la giocata dell'utente e la giocata del pc
-// e ne stabilisce il vincitore
-// function decreta_vincitore(user_number, pc_number) {
-//     if(user_number > pc_number) {
-//         console.log('Hai vinto!!');
-//     } else {
-//         console.log('Hai perso!!');
-//     }
-// }
